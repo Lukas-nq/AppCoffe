3ec50bbb348ede4a5e6a6b699517d0b3dcacd28c
Quanho12 <hhquan2102@gmail.com>
2026-05-21 09:27:25 +0700

    toisa dúabhdoasdsa

--- AppCoffe/UserControls/ucGoiMon.cs
+++ AppCoffe/UserControls/ucGoiMon.cs
@@ -16,5 +16,10 @@ namespace AppCoffe.UserControls
         {
             InitializeComponent();
         }
+
+        private void ucGoiMon_Load(object sender, EventArgs e)
+        {
+            helkodsaidndsadhnsoihdfiosahnfiosadoiwhqpi
+        }
     }
 }
