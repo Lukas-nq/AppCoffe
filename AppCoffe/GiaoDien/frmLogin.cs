99bc380f566e1bafc6f9469a29ddde417febeb95
nguyễn văn nam <namsuperme126@gmail.com>
2026-05-23 00:02:48 +0700

    chỉnh sửa form login trong file

--- AppCoffe/GiaoDien/frmLogin.cs
+++ AppCoffe/GiaoDien/frmLogin.cs
@@ -21,5 +21,15 @@ namespace AppCoffe.GiaoDien
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- AppCoffe/TestChucNang.Designer.cs
+++ AppCoffe/TestChucNang.Designer.cs
@@ -28,22 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ucQuanLyMenu1 = new AppCoffe.UserControls.ucQuanLyMenu();
             this.SuspendLayout();
-            // 
-            // ucQuanLyMenu1
-            // 
-            this.ucQuanLyMenu1.Location = new System.Drawing.Point(0, 0);
-            this.ucQuanLyMenu1.Name = "ucQuanLyMenu1";
-            this.ucQuanLyMenu1.Size = new System.Drawing.Size(879, 529);
-            this.ucQuanLyMenu1.TabIndex = 0;
             // 
             // TestChucNang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1132, 625);
-            this.Controls.Add(this.ucQuanLyMenu1);
+            this.ClientSize = new System.Drawing.Size(1132, 785);
             this.Name = "TestChucNang";
             this.Text = "TestChucNang";
             this.ResumeLayout(false);
@@ -51,7 +42,5 @@
         }
 
         #endregion
-
-        private UserControls.ucQuanLyMenu ucQuanLyMenu1;
     }
 }
