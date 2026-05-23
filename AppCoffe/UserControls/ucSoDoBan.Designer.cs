4bd8df7cf81f898d20b305739f58a6b4c10f1eb5
nguyễn văn nam <namsuperme126@gmail.com>
2026-05-23 15:10:30 +0700

    udate giao diện sodoban

--- AppCoffe/UserControls/ucSoDoBan.Designer.cs
+++ AppCoffe/UserControls/ucSoDoBan.Designer.cs
@@ -28,10 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBan1 = new System.Windows.Forms.Button();
+            this.btnBan2 = new System.Windows.Forms.Button();
+            this.btnBan3 = new System.Windows.Forms.Button();
+            this.btnBan4 = new System.Windows.Forms.Button();
+            this.btnBan5 = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.btnBan1);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan2);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan3);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan4);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan5);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(571, 400);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnBan1
+            // 
+            this.btnBan1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan1.BackColor = System.Drawing.Color.Green;
+            this.btnBan1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan1.ForeColor = System.Drawing.Color.White;
+            this.btnBan1.Location = new System.Drawing.Point(10, 10);
+            this.btnBan1.Margin = new System.Windows.Forms.Padding(10);
+            this.btnBan1.Name = "btnBan1";
+            this.btnBan1.Size = new System.Drawing.Size(110, 110);
+            this.btnBan1.TabIndex = 1;
+            this.btnBan1.Text = "Bàn 01";
+            this.btnBan1.UseVisualStyleBackColor = false;
+            this.btnBan1.Click += new System.EventHandler(this.btnBan_Click);
+            // 
+            // btnBan2
+            // 
+            this.btnBan2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan2.BackColor = System.Drawing.Color.Green;
+            this.btnBan2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan2.ForeColor = System.Drawing.Color.White;
+            this.btnBan2.Location = new System.Drawing.Point(140, 10);
+            this.btnBan2.Margin = new System.Windows.Forms.Padding(10);
+            this.btnBan2.Name = "btnBan2";
+            this.btnBan2.Size = new System.Drawing.Size(110, 110);
+            this.btnBan2.TabIndex = 2;
+            this.btnBan2.Text = "Bàn 02";
+            this.btnBan2.UseVisualStyleBackColor = false;
+            this.btnBan2.Click += new System.EventHandler(this.btnBan_Click);
+            // 
+            // btnBan3
+            // 
+            this.btnBan3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan3.BackColor = System.Drawing.Color.Green;
+            this.btnBan3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan3.ForeColor = System.Drawing.Color.White;
+            this.btnBan3.Location = new System.Drawing.Point(270, 10);
+            this.btnBan3.Margin = new System.Windows.Forms.Padding(10);
+            this.btnBan3.Name = "btnBan3";
+            this.btnBan3.Size = new System.Drawing.Size(110, 110);
+            this.btnBan3.TabIndex = 3;
+            this.btnBan3.Text = "Bàn 03";
+            this.btnBan3.UseVisualStyleBackColor = false;
+            this.btnBan3.Click += new System.EventHandler(this.btnBan_Click);
+            // 
+            // btnBan4
+            // 
+            this.btnBan4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan4.BackColor = System.Drawing.Color.Green;
+            this.btnBan4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan4.ForeColor = System.Drawing.Color.White;
+            this.btnBan4.Location = new System.Drawing.Point(400, 10);
+            this.btnBan4.Margin = new System.Windows.Forms.Padding(10);
+            this.btnBan4.Name = "btnBan4";
+            this.btnBan4.Size = new System.Drawing.Size(110, 110);
+            this.btnBan4.TabIndex = 4;
+            this.btnBan4.Text = "Bàn 04";
+            this.btnBan4.UseVisualStyleBackColor = false;
+            this.btnBan4.Click += new System.EventHandler(this.btnBan_Click);
+            // 
+            // btnBan5
+            // 
+            this.btnBan5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan5.BackColor = System.Drawing.Color.Green;
+            this.btnBan5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan5.ForeColor = System.Drawing.Color.White;
+            this.btnBan5.Location = new System.Drawing.Point(10, 140);
+            this.btnBan5.Margin = new System.Windows.Forms.Padding(10);
+            this.btnBan5.Name = "btnBan5";
+            this.btnBan5.Size = new System.Drawing.Size(110, 110);
+            this.btnBan5.TabIndex = 5;
+            this.btnBan5.Text = "Bàn 05";
+            this.btnBan5.UseVisualStyleBackColor = false;
+            this.btnBan5.Click += new System.EventHandler(this.btnBan_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(617, 336);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(139, 67);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Quay Lại";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // ucSoDoBan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "ucSoDoBan";
+            this.Size = new System.Drawing.Size(798, 531);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnBan1;
+        private System.Windows.Forms.Button btnBan2;
+        private System.Windows.Forms.Button btnBan3;
+        private System.Windows.Forms.Button btnBan4;
+        private System.Windows.Forms.Button btnBan5;
+        private System.Windows.Forms.Button btnBack;
     }
 }
--- AppCoffe/UserControls/ucSoDoBan.cs
+++ AppCoffe/UserControls/ucSoDoBan.cs
@@ -16,5 +16,41 @@ namespace AppCoffe.UserControls
         {
             InitializeComponent();
         }
+
+        private void btnBan_Click(object sender, EventArgs e)
+        {
+            Button btnSelected = sender as Button;
+            if (btnSelected == null) return;
+
+            // THIẾT LẬP CẤU TRÚC KIỂM TRA MÀU NỀN
+            // Trường hợp 1: Bàn đang TRỐNG (Màu Xanh) -> Khách vào ngồi
+            if (btnSelected.BackColor == Color.Green)
+            {
+                DialogResult r = MessageBox.Show($"Xác nhận chọn {btnSelected.Text}?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (r == DialogResult.Yes)
+                {
+                    // Thực thi lệnh đổi màu nền sang màu đỏ
+                    btnSelected.BackColor = Color.Red;
+                }
+            }
+            // Trường hợp 2: Bàn đang CÓ KHÁCH (Màu Đỏ) -> Khách về, dọn bàn
+            else if (btnSelected.BackColor == Color.Red)
+            {
+                DialogResult r = MessageBox.Show("Xác nhận dọn bàn?", "Cảnh báo an toàn", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (r == DialogResult.Yes)
+                {
+                    // Đưa màu nền nút bấm quay trở lại màu xanh
+                    btnSelected.BackColor = Color.Green;
+                }
+                // Nếu chọn [Hủy] (DialogResult.No), hệ thống tự giữ nguyên trạng thái cũ
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
     }
 }
