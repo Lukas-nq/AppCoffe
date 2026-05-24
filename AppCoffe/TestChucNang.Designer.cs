cc242d5f71e7b0a6e2754832982328e4ffc4c85a
nguyễn văn nam <namsuperme126@gmail.com>
2026-05-24 12:07:01 +0700

    Update TestChucNang.Designer.cs

--- AppCoffe/TestChucNang.Designer.cs
+++ AppCoffe/TestChucNang.Designer.cs
@@ -28,20 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBan1 = new System.Windows.Forms.Button();
+            this.btnBan2 = new System.Windows.Forms.Button();
+            this.btnBan3 = new System.Windows.Forms.Button();
+            this.btnBan4 = new System.Windows.Forms.Button();
+            this.btnBan5 = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.btnBan1);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan2);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan3);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan4);
+            this.flowLayoutPanel1.Controls.Add(this.btnBan5);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(64, 75);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(761, 492);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btnBan1
+            // 
+            this.btnBan1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan1.BackColor = System.Drawing.Color.Green;
+            this.btnBan1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan1.ForeColor = System.Drawing.Color.White;
+            this.btnBan1.Location = new System.Drawing.Point(13, 12);
+            this.btnBan1.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.btnBan1.Name = "btnBan1";
+            this.btnBan1.Size = new System.Drawing.Size(147, 135);
+            this.btnBan1.TabIndex = 1;
+            this.btnBan1.Text = "Bàn 01";
+            this.btnBan1.UseVisualStyleBackColor = false;
+            // 
+            // btnBan2
+            // 
+            this.btnBan2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan2.BackColor = System.Drawing.Color.Green;
+            this.btnBan2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan2.ForeColor = System.Drawing.Color.White;
+            this.btnBan2.Location = new System.Drawing.Point(186, 12);
+            this.btnBan2.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.btnBan2.Name = "btnBan2";
+            this.btnBan2.Size = new System.Drawing.Size(147, 135);
+            this.btnBan2.TabIndex = 2;
+            this.btnBan2.Text = "Bàn 02";
+            this.btnBan2.UseVisualStyleBackColor = false;
+            // 
+            // btnBan3
+            // 
+            this.btnBan3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan3.BackColor = System.Drawing.Color.Green;
+            this.btnBan3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan3.ForeColor = System.Drawing.Color.White;
+            this.btnBan3.Location = new System.Drawing.Point(359, 12);
+            this.btnBan3.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.btnBan3.Name = "btnBan3";
+            this.btnBan3.Size = new System.Drawing.Size(147, 135);
+            this.btnBan3.TabIndex = 3;
+            this.btnBan3.Text = "Bàn 03";
+            this.btnBan3.UseVisualStyleBackColor = false;
+            // 
+            // btnBan4
+            // 
+            this.btnBan4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan4.BackColor = System.Drawing.Color.Green;
+            this.btnBan4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan4.ForeColor = System.Drawing.Color.White;
+            this.btnBan4.Location = new System.Drawing.Point(532, 12);
+            this.btnBan4.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.btnBan4.Name = "btnBan4";
+            this.btnBan4.Size = new System.Drawing.Size(147, 135);
+            this.btnBan4.TabIndex = 4;
+            this.btnBan4.Text = "Bàn 04";
+            this.btnBan4.UseVisualStyleBackColor = false;
+            // 
+            // btnBan5
+            // 
+            this.btnBan5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBan5.BackColor = System.Drawing.Color.Green;
+            this.btnBan5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBan5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBan5.ForeColor = System.Drawing.Color.White;
+            this.btnBan5.Location = new System.Drawing.Point(13, 171);
+            this.btnBan5.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.btnBan5.Name = "btnBan5";
+            this.btnBan5.Size = new System.Drawing.Size(147, 135);
+            this.btnBan5.TabIndex = 5;
+            this.btnBan5.Text = "Bàn 05";
+            this.btnBan5.UseVisualStyleBackColor = false;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(883, 485);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(185, 82);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Quay Lại";
+            this.btnBack.UseVisualStyleBackColor = true;
             // 
             // TestChucNang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 522);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.ClientSize = new System.Drawing.Size(1132, 642);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnBack);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "TestChucNang";
             this.Text = "TestChucNang";
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnBan1;
+        private System.Windows.Forms.Button btnBan2;
+        private System.Windows.Forms.Button btnBan3;
+        private System.Windows.Forms.Button btnBan4;
+        private System.Windows.Forms.Button btnBan5;
+        private System.Windows.Forms.Button btnBack;
     }
 }
